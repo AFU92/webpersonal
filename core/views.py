f08583a8b62d85ffa1d6c6ad80593f6168e56ee2
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-08-04 22:23:32 -0500

    create templates

--- core/views.py
+++ core/views.py
@@ -1,44 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-
-<h1>My personal web</h1>
-
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about-me">About me</a></li>
-    <li><a href="/portafolio">Portafolio</a></li>
-    <li><a href="/contact">Contact</a></li>
-
-"""
 
 
 def home(request):
 
-    return HttpResponse(html_base+"""
-    <h2>Cover</h2>
-    <p>This is a cover baby</p>
-    """)
+    return render(request, "core/home.html")
 
 
 def about(request):
-    return HttpResponse(html_base+"""
-    <h2>About me</h2>
-    <p>Hello friend, my name is Andrea Fuentes and I'm software developer.</p>
-    """)
+
+    return render(request, "core/about-me.html")
 
 
 def portafolio(request):
 
-    return HttpResponse(html_base+"""
-    <h2>Portafolio</h2>About me
-    <p>This is a portafolio :)</p>
-    """)
+    return render(request, "core/portafolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base+"""
-    <h2>Contact</h2>
-    <p>My contact is: AFU92</p>
-    """)
+
+    return render(request, "core/contact.html")
