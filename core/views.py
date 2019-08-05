25c7a14ae1f951553c8c14b588881c03abc83454
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-08-04 21:54:21 -0500

    create portafolio and contact views and add urls

--- core/views.py
+++ core/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+html_base = """
+
+<h1>My personal web</h1>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about-me">About me</a></li>
+    <li><a href="/portafolio">Portafolio</a></li>
+    <li><a href="/contact">Contact</a></li>
+
+"""
 
 
 def home(request):
 
-    return HttpResponse("<h1>My personal web</h1><h2>Cover</h2>")
+    return HttpResponse(html_base+"""
+    <h2>Cover</h2>
+    <p>This is a cover baby</p>
+    """)
 
 
 def about(request):
-    return HttpResponse("<h1>My personal web</h1><h2>About</h2><p>Hello, my name is andrea</p>")
+    return HttpResponse(html_base+"""
+    <h2>About me</h2>
+    <p>Hello friend, my name is Andrea Fuentes and I'm software developer.</p>
+    """)
+
+
+def portafolio(request):
+
+    return HttpResponse(html_base+"""
+    <h2>Portafolio</h2>About me
+    <p>This is a portafolio :)</p>
+    """)
+
+
+def contact(request):
+    return HttpResponse(html_base+"""
+    <h2>Contact</h2>
+    <p>My contact is: AFU92</p>
+    """)
